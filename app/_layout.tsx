34b3449fcbbe518f51deceeac5de383a44038218
Francis Oke <francisokewale@gmail.com>
2025-04-12 17:27:57 +0100

    change status and navigation bar color for android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,21 @@
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import * as NavigationBar from 'expo-navigation-bar'
+import { Platform } from 'react-native'
 
 export default function RootLayout() {
+  if (Platform.OS === 'android') {
+    NavigationBar.setPositionAsync('absolute')
+    NavigationBar.setBackgroundColorAsync('#ffffff01') // To make in transparent
+    NavigationBar.setButtonStyleAsync('dark')
+  }
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" options={{ title: 'About' }} />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{ title: 'About' }} />
+      </Stack>
+    </>
   )
 }
