20fef1d081fbd1ce6070b846495e13c5a1614b35
Francis Oke <francisokewale@gmail.com>
2025-05-01 18:01:32 +0100

    update emoji modal

--- components/EmojiList.tsx
+++ components/EmojiList.tsx
@@ -1,8 +1,9 @@
+import { emojis } from '@/assets/images/images'
 import { Image, type ImageSource } from 'expo-image'
 import { useState } from 'react'
-import { FlatList, Platform, StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, View } from 'react-native'
 import { Button } from './Button'
-import { emojis } from '@/assets/images/images'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export interface EmojiListProps {
   onSelect: (emoji: ImageSource) => void
@@ -10,17 +11,20 @@ export interface EmojiListProps {
 
 export function EmojiList({ onSelect }: EmojiListProps) {
   const [emoji] = useState<ImageSource[]>(emojis)
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={emoji}
-        numColumns={5}
-        renderItem={({ item, index }) => (
-          <Button key={index} onPress={() => onSelect(item)}>
-            <Image source={item} style={styles.image} />
-          </Button>
-        )}
-      />
+      <ScrollView horizontal>
+        <FlatList
+          data={emoji}
+          numColumns={Math.ceil(emoji.length / 2)}
+          renderItem={({ item, index }) => (
+            <Button key={index} onPress={() => onSelect(item)}>
+              <Image source={item} style={styles.image} />
+            </Button>
+          )}
+        />
+      </ScrollView>
     </View>
   )
 }
@@ -31,7 +35,7 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   image: {
-    height: 25,
-    width: 25,
+    height: 50,
+    width: 50,
   },
 })
